67f7aa63e2f316a62bf39bc3b22993c4d7c4e4c0
DivineDemon <supame123@gmail.com>
2024-09-25 01:15:28 +0400

    Tape Animations!

--- src/sections/tape.tsx
+++ src/sections/tape.tsx
@@ -12,6 +12,17 @@ const words = [
   "Search Optimized",
   "Usable",
   "Reliable",
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Responsive",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
 ];
 
 export const TapeSection = () => {
@@ -19,7 +30,7 @@ export const TapeSection = () => {
     <div className="overflow-x-clip py-16 lg:py-24">
       <div className="-mx-1 -rotate-3 bg-gradient-to-r from-emerald-300 to-sky-400">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 py-3">
+          <div className="flex flex-none animate-move-left gap-4 py-3 [animation-duration:30s]">
             {words.map((word, idx) => (
               <div key={idx} className="inline-flex items-center gap-4">
                 <span className="text-sm font-bold uppercase text-gray-900">
