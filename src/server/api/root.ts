a9628156b0fb87dfb75f185db3d5caef7f204a8f
DivineDemon <supame123@gmail.com>
2025-01-06 17:49:10 +0500

    Shifted to T3 Stack!

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
-import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
+
+import { projectRouter } from "./routers/project";
+import { testimonialRouter } from "./routers/testimonial";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +9,8 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  project: projectRouter,
+  testimonial: testimonialRouter,
 });
 
 // export type definition of API
