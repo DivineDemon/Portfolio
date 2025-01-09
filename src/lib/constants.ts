514744454964acc989d92990094b344d7511dddf
DivineDemon <supame123@gmail.com>
2025-01-09 05:00:54 +0500

    Cleanup!

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,16 +4,6 @@ import GithubIcon from "@/assets/icons/github.svg";
 import HTMLIcon from "@/assets/icons/html5.svg";
 import ReactIcon from "@/assets/icons/react.svg";
 import JavScriptIcon from "@/assets/icons/square-js.svg";
-import CityCrm from "@/assets/images/city-crm.png";
-import CityWebsite from "@/assets/images/city.png";
-import DripWebsite from "@/assets/images/drip.png";
-import FCCms from "@/assets/images/fc-cms.png";
-import FCWebsite from "@/assets/images/fc-website.png";
-import memojiAvatar1 from "@/assets/images/memoji-avatar-1.png";
-import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
-import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
-import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
-import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 
 export const toolboxItems = [
   {
@@ -130,93 +120,6 @@ export const footerLinks = [
   },
 ];
 
-export const portfolioProjects = [
-  {
-    company: "F&C Properties",
-    year: "2023",
-    title: "Company Website",
-    results: [
-      {
-        title: "Achieved a 51.7% increase in website traffic and impressions.",
-      },
-      {
-        title:
-          "Achieved a 23.3% increase in quality of organic real-estate leads.",
-      },
-      { title: "Achieved 16% growth for Company Revenue." },
-    ],
-    link: "https://fandcproperties.ae",
-    image: FCWebsite,
-  },
-  {
-    company: "City Doctor",
-    year: "2023",
-    title: "Company Website",
-    results: [
-      {
-        title: "Achieved a 22.6% increase in website traffic and impressions.",
-      },
-      { title: "Expanded customer reach by 35%." },
-      { title: "Implemented Advanced SEO techniques." },
-    ],
-    link: "https://citydoctor.vercel.app",
-    image: CityWebsite,
-  },
-  {
-    company: "Driphvb",
-    year: "2023",
-    title: "Company Website",
-    results: [
-      {
-        title: "Achieved a 28.3% increase in website traffic and impressions.",
-      },
-      { title: "Implemented Advanced SEO techniques." },
-      { title: "Increased mobile traffic by 44%." },
-    ],
-    link: "https://driphvb.vercel.app",
-    image: DripWebsite,
-  },
-  {
-    company: "F&C Properties",
-    year: "2023",
-    title: "CMS",
-    results: [
-      {
-        title:
-          "Achieved a 112% increase in data addition and processing speeds.",
-      },
-      {
-        title:
-          "Achieved a 9.4% increase in quality of organic real-estate leads.",
-      },
-      {
-        title:
-          "Reduced Firm Data Management Costs by approximately $50k/month.",
-      },
-    ],
-    link: "https://cms-fandcproperties.vercel.app",
-    image: FCCms,
-  },
-  {
-    company: "City Doctor",
-    year: "2023",
-    title: "CRM",
-    results: [
-      {
-        title: "Boosted scheduling efficiency by 40% in six months.",
-      },
-      {
-        title: "Reduced manual data entry by 60%.",
-      },
-      {
-        title: "Increased repeat customer engagement by 20%.",
-      },
-    ],
-    link: "https://city-crm.vercel.app",
-    image: CityCrm,
-  },
-];
-
 export const words = [
   "Performant",
   "Accessible",
@@ -240,37 +143,4 @@ export const words = [
   "Search Optimized",
   "Usable",
   "Reliable",
-];
-
-export const testimonials = [
-  {
-    name: "Qasim Irshaad",
-    position: "Tech Lead @ Digimark",
-    text: "Mushood was instrumental in transforming our frontend team into a powerful workforce. His attention to detail and ability to understand client needs is exceptional. We're thrilled with the results!",
-    avatar: memojiAvatar1,
-  },
-  {
-    name: "Farrukh Iminov",
-    position: "CEO @ F&C Properties",
-    text: "Working with Mushood was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
-    avatar: memojiAvatar2,
-  },
-  {
-    name: "Irfan Saleem",
-    position: "CEO @ Intech Studios",
-    text: "Mushood's ability to create seamless user experiences is unmatched. Our company has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
-    avatar: memojiAvatar3,
-  },
-  {
-    name: "Ahmed Elnakewi",
-    position: "CEO @ City Doctor",
-    text: "Mushood is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
-    avatar: memojiAvatar4,
-  },
-  {
-    name: "Raghavendra Ghuge",
-    position: "CEO @ AIOMEGA",
-    text: "Mushood is an exceptional backend developer. He expertly built our product's core systems, ensuring robust performance, security, and scalability. His work has significantly enhanced our platform's stability and efficiency.",
-    avatar: memojiAvatar5,
-  },
 ];
